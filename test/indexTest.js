4484107d54b5437b8e7a7610073a083bc55493fe
samk <samuelkariuki1@student.moringaschool.com>
2023-06-12 07:20:15 +0300

    unable

--- test/indexTest.js
+++ test/indexTest.js
@@ -4,7 +4,7 @@ const { name, height, message } = require("../index.js");
 
 describe("what-is-a-test", () => {
   describe("Name", () => {
-    it('returns "Susan"', () => {
+    it('returns "susan"', () => {
       expect(name).toEqual("Susan");
     });
   });
